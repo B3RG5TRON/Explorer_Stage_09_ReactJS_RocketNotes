2780701756f775758daa078defcd516d268c0cbd
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-06-06 20:54:15 -0300

    ✨ Commit: 'Profile' Page Created

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,62 @@
+import { Container, Form, Avatar } from "./styles";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft />
+        </a>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img
+            src="https://github.com/B3RG5TRON.png"
+            alt="Foto do usuário"
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input
+              type="file"
+              id="avatar"
+            />
+          </label>
+        </Avatar>
+
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
+    </Container>
+  )
+}
