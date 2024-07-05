e4f9fd4cfe27a3b55e7429cac414677bf13e61f6
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-07-05 14:14:53 -0300

    :lock: Commit: Enhancing Authentication Hooks

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
+import { useAuth } from "../../hooks/auth";
 
 import { Link } from "react-router-dom";
 
@@ -8,6 +10,15 @@ import { Button } from "../../components/Button";
 import { FiMail, FiLock } from "react-icons/fi";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {signIn} = useAuth()
+
+  function handleSignIn() {
+    signIn({email, password})
+  }
+
   return (
     <Container>
       <Form>
@@ -20,20 +31,22 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
-        </Form>
+      </Form>
 
-        <Background />
+      <Background />
     </Container>
   );
 }
