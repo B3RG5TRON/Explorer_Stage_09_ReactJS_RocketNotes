5aff81feaa0d5736766ed52169521e2c88820b07
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-08-27 21:27:23 -0300

    🚪Commit: Implement SignOut Function

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -28,13 +28,20 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem("@rocketnotes:token");
+    localStorage.removeItem("@rocketnotes:user");
+
+    setData({});
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token");
     const user = localStorage.getItem("@rocketnotes:user");
 
     if (token && user) {
       api.defaults.headers.authorization = `Bearer ${token}`;
-      
+
       setData({
         token,
         user: JSON.parse(user)
@@ -43,7 +50,11 @@ function AuthProvider({ children }) {
   }, [])
 
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{
+      signIn,
+      signOut,
+      user: data.user
+    }}>
       {children}
     </AuthContext.Provider>
   );
