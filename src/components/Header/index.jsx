73888f3e223abf3e6d0c67a66f6ff59a8de1d584
Eduardo Bergstron <eduardo.goudinho@gmail.com>
2024-12-04 23:42:34 -0300

    Commit: Handle unavailable routes and add logout route within notes 🚫🔒

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
-import { useAuth } from '../../hooks/auth'
+import { useNavigate } from 'react-router-dom'
 
+import { useAuth } from '../../hooks/auth'
 import { api } from '../../services/api'
 
 import { RiShutDownLine } from 'react-icons/ri'
@@ -9,6 +10,12 @@ import { Container, Profile, Logout } from './styles'
 
 export function Header() {
   const { signOut, user } = useAuth()
+  const navigation = useNavigate()
+
+  function handleSignOut() {
+    navigation('/')
+    signOut()
+  }
 
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
 
@@ -26,7 +33,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
